c429da1c5e5a54b9aa309c6b6d43484e959af308
Carlos Rabelo <developer@carlosrabelo.com.br>
2021-11-04 20:05:00 -0400

    feat: export public package API

--- biorhythm/__init__.py
+++ biorhythm/__init__.py
@@ -1,3 +1,17 @@
 """Biorhythm calculator — physical, emotional, and intellectual cycles."""
 
+from .errors import BiorhythmError, FutureBirthDateError, InvalidBirthDateError
+from .processor import BiorhythmResult, calculate_biorhythm, compute, parse_date
+
 __version__ = "0.1.0"
+
+__all__ = [
+    "BiorhythmError",
+    "BiorhythmResult",
+    "FutureBirthDateError",
+    "InvalidBirthDateError",
+    "calculate_biorhythm",
+    "compute",
+    "parse_date",
+    "__version__",
+]
